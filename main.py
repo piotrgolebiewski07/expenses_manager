dbd78442aaf7a40706c13cdbc47130f0d00f8dd8
Piotr Golebiewski <foto-max.net@wp.pl>
2024-12-30 16:47:28 +0100

    Refractor create and get expenses endpoints

--- main.py
+++ main.py
@@ -1,64 +1,65 @@
-from fastapi import Body, FastAPI
+import datetime
+
+from pydantic import BaseModel
+from fastapi import FastAPI
 
 
 app = FastAPI()
 
 
-list_of_expenses = [
-    {"id": "1", "expense": "milk", "category": "food", "price": 10},
-    {"id": "2", "expense": "bread", "category": "food", "price": 5},
-    {"id": "3", "expense": "water", "category": "fees", "price": 150},
-    {"id": "4", "expense": "phone", "category": "fees", "price": 20}
-    ]
+class ExpenseCreateDTO(BaseModel):
+    name: str
+    category: str
+    price: int
 
 
-@app.get("/list-of-expenses")
-def read_all_expenses():
-    return list_of_expenses
+class ExpenseDTO(BaseModel):
+    id: int
+    name: str
+    category: str
+    price: int
+    datetime: datetime.datetime
+
+    class Config:
+        from_attributes = True
 
 
-@app.get("/list_of_expenses/{expenses_id}")
-def read_expenses_category(expenses_id: str):
-    for x in list_of_expenses:
-        if x.get('id') == expenses_id:
-            return x
+class Expense:
+
+    def __init__(self, id: int, expense: str, category: str, price: int):
+        self.id = id
+        self.name = expense
+        self.category = category
+        self.price = price
+        self.datetime = datetime.datetime.now()
 
 
-@app.get("/list_of_expenses/{category}")
-def read_category_by_query(category: str):
-    expenses = []
-    for x in list_of_expenses:
-        if x.get("category") == category:
-            expenses.append(x)
-    return expenses
+EXPENSES = [
+    Expense(1, "vegetables", "food", 40),
+    Expense(2, "fruit", "food", 30),
+    Expense(3, "phone", "fees", 70),
+    Expense(4, "internet", "food", 200),
+    Expense(5, "clothes", "shopping", 500)
+]
 
 
-@app.get("/list_of_expenses/{price}")
-def read_price_by_query(price: int):
-    list_of_price = []
-    for x in list_of_expenses:
-        if x.get("price") == price:
-            list_of_price.append(x)
-    return list_of_price
+@app.get("/expenses/", response_model=list[ExpenseDTO])
+def read_expense_endpoint():
+    return EXPENSES
 
+@app.post("/expenses/", response_model=ExpenseDTO)
+def create_expense_endpoint(dto: ExpenseCreateDTO):
+    print(dto)
 
-@app.post("/list_of_expenses/create_expense")
-def create_expense(new_expense=Body()):
-    list_of_expenses.append(new_expense)
+    # mock dodawanie do bazy danych
+    expense = Expense(
+        id=len(EXPENSES) + 1,
+        expense=dto.name,
+        category=dto.category,
+        price=dto.price
+    )
+    EXPENSES.append(expense)
 
-
-@app.put("/list_of_expenses/update_expense")
-def update_expense(updated_expense=Body()):
-    for i in range(len(list_of_expenses)):
-        if list_of_expenses[i].get("id") == updated_expense.get("id"):
-            list_of_expenses[i] = updated_expense
-
-
-@app.delete("/list_of_expenses/delete_expense/{expense_id}")
-def delete_expense(expense_id: str):
-    for i in range(len(list_of_expenses)):
-        if list_of_expenses[i].get("id") == expense_id:
-            list_of_expenses.pop(i)
-            break
-
-
+    # output
+    dto = ExpenseDTO.from_orm(expense)
+    return dto
