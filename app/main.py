6bdd083ff6ac5771935ca50c07cf1efaa47081b2
Piotr Golebiewski <pgolebiewski07@gmail.com>
2026-02-19 15:18:52 +0100

    Refactor error handling: introduce custom exceptions, clean router and CRUD layers

--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from app.expenses.routers import router as expenses_router
 from app.expenses.database import engine
 from app.expenses.models import Base
 
+from app.core.exception import ExpenseNotFoundException, NoExpensesFoundException, DatabaseException, \
+    InvalidMonthException
 
-async def lifespan(app: FastAPI):
 
-    # Tworzenie tabel w bazie danych
+@asynccontextmanager
+async def lifespan(_app: FastAPI):
     Base.metadata.create_all(bind=engine)
     print("Starting app ...")
     yield
@@ -20,5 +24,37 @@ app = FastAPI(
     lifespan=lifespan
 )
 
+
+@app.exception_handler(ExpenseNotFoundException)
+async def expense_not_found_handler(request: Request, exc: ExpenseNotFoundException):
+    return JSONResponse(
+        status_code=404,
+        content={"detail": "Expense not found"}
+    )
+
+
+@app.exception_handler(NoExpensesFoundException)
+async def no_expenses_found_handler(request: Request, exc: NoExpensesFoundException):
+    return JSONResponse(
+        status_code=404,
+        content={"detail": "No expenses found"}
+    )
+
+
+@app.exception_handler(DatabaseException)
+async def database_exception_handler(request: Request, exc: DatabaseException):
+    return JSONResponse(
+        status_code=500,
+        content={"detail": "Database error"}
+    )
+
+
+@app.exception_handler(InvalidMonthException)
+async def invalid_month_handler(request: Request, exc: InvalidMonthException):
+    return JSONResponse(
+        status_code=400,
+        content={"detail": "Month must be between 1 and 12"}
+
+    )
 app.include_router(expenses_router)
 
