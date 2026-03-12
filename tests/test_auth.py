c0ecd72c49c471cdf317cc92189849280fa382a2
Piotr Golebiewski <pgolebiewski07@gmail.com>
2026-03-12 18:53:47 +0100

    Refactor auth login tests structure

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,37 +1,43 @@
-def test_auth_login_fail(client):
-    response = client.post("/auth/login",
-                           json={
-                               "email": "wrong@example.com",
-                               "password": "wrong_password"
-                           })
+# -----------------------
+# Login
+# -----------------------
+class TestAuthLogin:
 
-    assert response.status_code == 401
+    def test_auth_login_fail(self, client):
+        response = client.post("/auth/login",
+                               json={
+                                   "email": "wrong@example.com",
+                                   "password": "wrong_password"
+                               })
+
+        assert response.status_code == 401
+
+    def test_auth_login_success(self, client, test_user):
+        response = client.post(
+            "/auth/login",
+            json={
+                "email": "test@example.com",
+                "password": "Aaaaaa12"
+            }
+        )
+
+        assert response.status_code == 200
+        data = response.json()
+
+        assert "access_token" in data
+        assert isinstance(data["access_token"], str)
+        assert data["token_type"] == "bearer"
+
+    def test_auth_login_wrong_password(self, client, test_user):
+        response = client.post(
+            "/auth/login",
+            json={
+                "email": "test@example.com",
+                "password": "wrong_password"
+            }
+        )
+
+        assert response.status_code == 401
 
 
-def test_auth_login_success(client, test_user):
-    response = client.post(
-        "/auth/login",
-        json={
-            "email": "test@example.com",
-            "password": "Aaaaaa12"
-        }
-    )
-
-    assert response.status_code == 200
-    data = response.json()
-
-    assert "access_token" in data
-    assert isinstance(data["access_token"], str)
-
-
-def test_auth_login_wrong_password(client, test_user):
-    response = client.post(
-        "/auth/login",
-        json={
-            "email": "test@example.com",
-            "password": "wrong_password"
-        }
-    )
-
-    assert response.status_code == 401
 
